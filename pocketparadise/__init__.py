b12fd1ce5ccefe9fafab9d1d6505f7ab63e139ae
AlexchooN14 <33004020+AlexchooN14@users.noreply.github.com>
2022-03-06 23:03:31 +0200

    RE-configured init for better database usage

--- pocketparadise/__init__.py
+++ pocketparadise/__init__.py
@@ -1,16 +1,24 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import Config
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
 
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '01b60x1d18a79ddfasw03dxda54adf7314'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
 
-app = Flask(__name__, template_folder='../templates')
-app.config['SECRET_KEY'] = Config.SECRET_KEY
-app.config['SQLALCHEMY_DATABASE_URI'] = Config.SQLALCHEMY_DATABASE_URI
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-db = create_engine(Config.SQLALCHEMY_DATABASE_URI)
-Base = declarative_base()
-Session = sessionmaker(db)
-session = Session()
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASSWORD')
+mail = Mail(app)
+
+from pocketparadise import routes
